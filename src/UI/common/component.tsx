30e33c99868368881efd373e6189305373e89c9c
Oleg-Borovik <ob.borovick@mail.ru>
2022-07-30 21:40:00 +0300

    first commit

---
+++ src/UI/common/component.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const Text: React.FC = () => {
+    return <div>
+        Какаой-то текст 3123123 
+        <input type="text" />
+    </div>
+}
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from './style.less'
+import {Text} from './UI/common/component'
+
+const App = () => {
+    return <div className={styles.div}>
+        СДЕЛАЙ СУКА ДЕЛО
+        <Text/>
+    </div>
+}
+export default App;
